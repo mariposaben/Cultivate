e0bef03b3539068e4418b6d5fd67c5cbe0ec5c62
Ben Pierce <mariposafamily@gmail.com>
2021-02-06 09:49:02 -0800

    Created base database infrastruce solution/project

---
+++ Infrastructure/Database/CultivateSeed.Infrastructure.Database/ConnectionStrings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CultivateSeed.Infrastructure.Database
+{
+    public class ConnectionStrings
+    {
+        public string Cultivate { get; set; }
+        public string CultivateReference { get; set;}
+        public string CultivateReporting { get; set;}
+    }
+}
